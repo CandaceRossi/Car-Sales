e8938bc9a79c2be576de0f9c9810df7a0594a78f
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-09-11 17:10:52 -0400

    built a createStore added props to provider wrapped app, created carReducer and passed reducer into provider as prop

---
+++ src/reducers/carReducer.js
@@ -0,0 +1,11 @@
+export const initialState = {
+    name="",
+    price=""
+}
+export const carReducer = (state = initialState, action) => {
+switch(action.type) {
+    default:
+        return state;
+}
+
+}
